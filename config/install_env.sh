24704bc830b56ed68b80e9b67f0e2cd38798a1e9
sunke nithin <nsunke@outlook.com>
2024-02-16 08:33:27 +0530

    dsZ

--- config/install_env.sh
+++ config/install_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DOMAIN_NAME=scs.com
 export HOSTNAME=sddoclrn
-export public_ip=192.168.0.26
+export public_ip=192.168.3.26
 export DB_DOMAIN=scs.com
 export ROOT_PASSWORD=Nithin#0712
 export NSUNKE_PASSWORD=Nithin#0712

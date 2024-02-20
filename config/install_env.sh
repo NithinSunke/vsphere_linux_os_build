79f2cf91dcadedc902efaace5704172b6db9bee0
sunke nithin <nsunke@outlook.com>
2024-02-20 21:48:27 +0530

    sd

--- config/install_env.sh
+++ config/install_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DOMAIN_NAME=scs.com
-export HOSTNAME=soel701
-export public_ip=192.168.3.27
+export HOSTNAME=oeldb19c2
+export public_ip=192.168.3.28
 export DB_DOMAIN=scs.com
 export ROOT_PASSWORD=Nithin#0712
 export NSUNKE_PASSWORD=Nithin#0712

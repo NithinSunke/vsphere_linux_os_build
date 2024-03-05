044f332e21132b030e45d19c4a966d250cb9e8f7
sunke nithin <nsunke@outlook.com>
2024-03-05 05:35:11 +0530

    sdSz

--- config/install_env.sh
+++ config/install_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export DOMAIN_NAME=scs.com
-export HOSTNAME=mylabca
-export public_ip=192.168.3.11
+export DOMAIN_NAME=sdoc01
+export HOSTNAME=gitlab
+export public_ip=192.168.3.30
 export DB_DOMAIN=scs.com
 export ROOT_PASSWORD=Nithin#0712
 export NSUNKE_PASSWORD=Nithin#0712
